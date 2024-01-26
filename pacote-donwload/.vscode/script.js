4ba829e8e5d2a04a5897f9028b779ec4370215e2
Luiz Gabriel <gabrielaquinostedile@gmail.com>
2024-01-26 19:01:43 -0300

    commit full project

---
+++ pacote-donwload/.vscode/script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+const html = document.documentElement
+html.classList.toggle('light')
+//pegar a img
+const img = document.querySelector("#profile img")
+//substituir a img
+
+if(html.classList.contains('light')){
+//se tiver light mode adicionar a img light
+img.setAttribute('src', './assets/avatar-light.png.png')
+} else{
+//se tiver sem light mode manter a imagem normal
+img.setAttribute('src', './assets/avatar.png.png')
+}
+}
